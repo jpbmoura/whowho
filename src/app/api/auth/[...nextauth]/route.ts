cdc8aee145d562b848a660653a51703daef7cdf9
jpbmoura <jopmoura49@gmail.com>
2024-06-20 17:54:48 -0300

    fix build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
-
-const scope =
-  "user-read-recently-played user-top-read user-read-currently-playing user-read-email user-read-private";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID as string,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
-      authorization: {
-        params: { scope },
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.id = account.id;
-        token.expires_at = account.expires_at;
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.user = token;
-      return session;
-    },
-  },
-  pages: {
-    signIn: "/login",
-  },
-};
+import { authOptions } from "@/utils/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
--- src/utils/server-utils.tsx
+++ src/utils/server-utils.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions";
 import { AuthSession } from "@/types/types";
 import { getServerSession } from "next-auth/next";
 
