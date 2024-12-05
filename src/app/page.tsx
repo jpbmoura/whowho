eb549576ed8ab471860e11d23892bdaf47aabbc4
jpbmoura <jopmoura49@gmail.com>
2024-12-05 12:53:33 -0300

    add loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { SignedOut, SignInButton } from "@clerk/nextjs";
+import { SignedOut, SignInButton, useSession } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default function Root() {
+  const session = useSession();
+
+  if (session.isSignedIn) {
+    redirect("/home/album");
+  }
+
   return (
     <>
       <div className="flex justify-center items-center h-screen ">
+        {!session.isLoaded && (
+          <div className="text-7xl font-koulen animate-pulse">W</div>
+        )}
         <SignedOut>
           <SignInButton forceRedirectUrl="/home/album">
             <Button>Sign In</Button>
