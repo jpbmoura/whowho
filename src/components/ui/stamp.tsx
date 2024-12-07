2aefc47af186a7fb7fcd57f6734a2feac32d7e6b
jpbmoura <jopmoura49@gmail.com>
2024-12-06 23:52:35 -0300

    add hover on stamps

--- src/components/ui/stamp.tsx
+++ src/components/ui/stamp.tsx
@@ -5,7 +5,9 @@ interface StampProps {
 const Stamp = ({ children }: StampProps) => {
   return (
     <div className="relative stamp-container">
-      <div className="stamp bg-white border w-36">{children}</div>
+      <div className="stamp bg-white border w-36 hover:bg-zinc-400 transition-colors">
+        {children}
+      </div>
     </div>
   );
 };
